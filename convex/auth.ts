c810e93ecbb4ae57ec3e5c4309cfa1ccf239d712
lukaspozdisek <lukas.pozdisek@gmail.com>
2026-02-04 17:23:34 +0100

    Update auth.ts

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,7 +1,14 @@
 import { convexAuth } from "@convex-dev/auth/server"
 import { ResendOTP } from "./ResendOTP"
 import { Password } from "@convex-dev/auth/providers/Password"
+// 1. Importujte Google providera
+import Google from "@auth/core/providers/google"
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [ResendOTP, Password({ reset: ResendOTP, verify: ResendOTP })],
+  // 2. Přidejte Google do seznamu (automaticky si vezme AUTH_GOOGLE_ID a SECRET z environment variables)
+  providers: [
+    Google, 
+    ResendOTP, 
+    Password({ reset: ResendOTP, verify: ResendOTP })
+  ],
 })
